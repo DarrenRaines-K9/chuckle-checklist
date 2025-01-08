98c6aa78e8afc17e09211d9cb4840ef223957d71
Darren Raines <draines.123123@gmail.com>
2025-01-08 15:22:43 -0600

    told and untold jokes displaying properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,78 @@
 import { useEffect, useState } from "react"
-import { createNewJoke, getAllJokes } from "./services/jokeService.js"
+import { createNewJoke, getAllJokes} from "./services/jokeService.js"
 import "./App.css"
 import stevePic from "./assets/steve copy.png"
 
 export const App = () => {
   const [newJoke, setNewJoke] = useState([])
   const [allJokes, setAllJokes] = useState([])
+  const [toldJokes, setToldJokes] = useState([])
+  const [untoldJokes, setUntoldJokes] = useState([])
+
 
   const transientState = {
 
     "id": 0,
     "text": newJoke,
-    "told": false
+    "told": false,
 
-}
+  }
 
-  useEffect(() => {
-    getAllJokes().then((jokeArray) => {
+  const jokeListData = () => {
+      getAllJokes().then((jokeArray) => {
       setAllJokes(jokeArray)
     })
-  }, [])
- 
-  const handleBtnClick = () => {
-    createNewJoke(transientState)
-    setNewJoke("")
-    console.log("clicked")
+
   }
+
+  useEffect(() => {
+    jokeListData()
+  }, [newJoke])
+
+  useEffect(() => {
+    if (allJokes.length > 0) {
+
+      setToldJokes(allJokes.filter(joke => joke.told === true))
+      setUntoldJokes(allJokes.filter(joke => joke.told === false))
+
+    }
+  }, [allJokes])
 
   return <div className="app-heading">
     <div className="app-heading-circle">
-   <img className="app-logo" src={stevePic} alt="Good job Steve" />
- </div>
+      <img className="app-logo" src={stevePic} alt="Good job Steve" />
+    </div>
     <div className="app-heading-text">Chuckle Checklist!</div>
     <div>
-     <div className="joke-add-form">
-      <input className="joke-input" type="text" placeholder="New One Liner" value={newJoke} onChange={(event) => {
-        // What's the value of event?
-        setNewJoke(event.target.value)
-      }} />
-      <button className="joke-input-submit" onClick={(event) => {handleBtnClick(event.target.value)}}>Add</button>
+      <div className="joke-add-form">
+        <input className="joke-input" type="text" placeholder="New One Liner" value={newJoke} onChange={(event) => {
+          setNewJoke(event.target.value)
+        }} />
+        <button className="joke-input-submit" onClick={(event) => { createNewJoke(transientState), setNewJoke("") }}>Add</button>
+      </div>
+      <div className="joke-lists-container">
+        <header className="joke-lists-container">Told</header>
+        <div >
+          {toldJokes.map(joke => {
+            return (
+              <div className="joke-list-item" key={joke.id}>
+                <div className="joke-list-item-text">{joke.text}</div>
+              </div>
+            )
+          })}
+        </div>
+        <div>
+          <header className="joke-lists-container">UnTold</header>
+          <div >
+            {untoldJokes.map(joke => {
+              return (
+                <div className="joke-list-item" key={joke.id}>
+                  <div className="joke-list-item-text">{joke.text}</div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
       </div>
     </div>
   </div>
